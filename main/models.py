a7d6c5313e37d1987468ad19378207c439832d37
dafandikri <dafandikri@gmail.com>
2024-09-16 01:22:47 +0700

    Refactor models and templates, added forms

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+import uuid
 
-class Item(models.Model):
+class ItemEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
